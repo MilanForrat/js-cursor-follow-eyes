9f9fb557a18f86aff34d6a86f04e6ca9bdc2a9dd
MilanForrat <you@example.commilan.forrat@gmail.com>
2021-06-07 23:19:24 +0200

    feat: robot image

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ document.onmousemove = function (){
     let x = event.clientX * 100 / window.innerWidth + "%";
     let y = event.clientY * 100 / window.innerHeight + "%";
 
+    // on cherche à associer la position du curseur avec celle des yeux, on boucle par 2 car on a deux éléments à itérer : eye balls
     for(let i = 0; i <2;i++){
         eyeBalls[i].style.left = x;
         eyeBalls[i].style.top = y;
-        eyeBalls[i]
+        // on translate en sens inverse pour éviter que les yeux sortent du cadre ou descendent trop bas par exemple
+        eyeBalls[i].style.transform = "translate(-"+x+",-"+y+")";
     }
 }
